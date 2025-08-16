a0185f44997d7ecb1453c1b7822a537c4355d948
Donia Ben Sedrine <bensedrinedonia@gmail.com>
2025-08-16 17:21:52 +0100

    added the optimized code

---
+++ day5foundry/src/BatchTransferOp.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.13;
+
+contract BatchTransferOp {
+
+   function SendEthOp(address []  calldata recipients, uint256 amount) external paybale{
+      uint256 length == recipients.length;
+      require(msg.value == length * amount, "incorrect Eth sent");
+
+      for (uint256 i; i < length;){
+         (bool sent,) = recipients[i].call{value: amount}("");
+         require(sent, "Transfer failed");
+         unchecked{i++;}; // save gas on increment
+   }
+
+    
+
+} 
