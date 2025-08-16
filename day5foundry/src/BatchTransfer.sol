d235dbcdf791104f6843cc734b8de8554ec0a679
Donia Ben Sedrine <bensedrinedonia@gmail.com>
2025-08-16 17:05:20 +0100

    transfer eth without optimization

--- day5foundry/src/BatchTransfer.sol
+++ day5foundry/src/BatchTransfer.sol
@@ -1,5 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract BatchTransfer {}
+contract BatchTransfer {
+
+    function TransferEth (address [] calldata recipients, uint256 amount) external payable {
+        require (msg.value == recipients.length * amount, "incorrect Eth sent")
+        for(uint256 i = 0; i < recipients.length; i++){
+            payable(recipients[i].transfer(amount))
+
+        }
+    }
+
+} 
    
