0bbde819a8ec915cbdc214b24044ec8ef3bad892
Donia Ben Sedrine <bensedrinedonia@gmail.com>
2025-08-24 16:22:12 +0100

    fixing directory

--- test/Day11.t.sol
+++ test/Day11.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
-import {SimpleBank} from "../src/SimpleBank.sol";
+import {Test, console} from "forge-std/Day11.sol";
+import {SimpleBank} from "../day11foundry/src/SimpleBank.sol";
 
 contract SimpleBankTest is Test {
     SimpleBank bank;
