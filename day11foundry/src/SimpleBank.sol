d8316a7ef132e43a9a35c9d2bef30f93be2bd91c
Donia Ben Sedrine <bensedrinedonia@gmail.com>
2025-08-24 15:53:28 +0100

    updated the smart contract

--- day11foundry/src/SimpleBank.sol
+++ day11foundry/src/SimpleBank.sol
@@ -4,17 +4,21 @@ pragma solidity ^0.8.13;
 contract SimpleBank {
     mapping(address => uint256) private balances;
 
+    // Deposit ETH to the bank
     function deposit() external payable {
-        require(msg.value > 0, "Deposit must be > 0");
+        require(msg.value > 0, "Must deposit more than 0");
         balances[msg.sender] += msg.value;
     }
 
+    // Withdraw ETH from the bank
     function withdraw(uint256 amount) external {
-        require(balances[msg.sender] >= amount, "Not enough balance");
+        require(amount > 0, "Withdraw amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
         balances[msg.sender] -= amount;
         payable(msg.sender).transfer(amount);
     }
 
+    // Check balance of the caller
     function getBalance() external view returns (uint256) {
         return balances[msg.sender];
     }
